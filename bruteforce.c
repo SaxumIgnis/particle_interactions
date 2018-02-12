f7531e9988223c62baa83063239700ab844045e6
SaxumIgnis <silvere.du-gardin@polytechnique.edu>
2018-02-13 00:31:49 +0100

    Create bruteforce.c

---
+++ bruteforce.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <math.h>
+#include <sys/time.h>
+#include <assert.h>
+#include <unistd.h>
+
+#include "ui.h"
+#include "nbody.h"
+#include "nbody_tools.h"
+
+void main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+  
+  if 
+}
